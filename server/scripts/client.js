b6d90cbb15fbd3c8db3a079907590c4879d1c8fb
Hailee Miu <hailee.miu@gmail.com>
2018-09-19 16:21:23 -0500

    add shoes get request

---
+++ server/scripts/client.js
@@ -0,0 +1,20 @@
+// const shoeApp = angular.module('ShoeApp', []);
+
+// taskApp.controller('TaskController', ['$http', function ($http) {
+//   const vm = this;
+
+//   vm.shoeData = [];
+
+//     // GET
+//     vm.getTasks = function () {
+//       $http({
+//         method: 'GET',
+//         url: '/shoes'
+//       }).then(function () {
+//         vm.shoeData = results.rows;
+//       }).catch(function () {
+//         alert('unable to get shows');
+//       })
+//     }
+
+// }])
--- server/server.js
+++ server/server.js
@@ -1,15 +1,17 @@
 const pg = require('pg');
-
+const express = require('express');
+const app = express();
 const Pool = pg.Pool;
+const port = process.env.PORT || 5000;
 
 // Set up to connect to database
 const pool = new Pool({
   // name of database
-  database: 'shoe_store', 
+  database: 'shoe_store',
   // will change on deployment
-  host: 'localhost', 
+  host: 'localhost',
   // postgres default port
-  port: '5432', 
+  port: '5432',
   // number of requests at one time
   max: 10,
   // 30 seconds
@@ -19,20 +21,28 @@ const pool = new Pool({
 // optional 
 pool.on('connect', () => {
   console.log('postgresql connected!');
-}); 
+});
 
 // optional
 pool.on('error', (error) => {
-  consol.log('error with postgresql pool:', error)
+  consol.log('error with postgresql pool:', error);
 });
 
-// connect
-// name of table "shoes"
-pool.query('SELECT * FROM "shoes";')
-  // does not have be to be "results"
-  .then((results) => {
-    console.log(results.rows);
-  })
-  .catch((error) => {
-    console.log('error with SQL select for shoes', error);
-  })
+// spin up server
+app.listen(port, () => {
+  console.log('server up on:', port);
+}) // end spin up server
+
+app.get('/shoes', (req, res) => {
+  // connect
+  // name of table "shoes"
+  pool.query('SELECT * FROM "shoes";')
+    // does not have be to be "results"
+    .then((results) => {
+      res.send(results.rows);
+    })
+    .catch((error) => {
+      console.log('error with SQL select for shoes', error);
+      res.sendStatus(500);
+    })
+})
